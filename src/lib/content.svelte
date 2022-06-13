0517b9b02eb6004d43ed984641166a4165350864
HanDaWoon <bookpia.han@gmail.com>
2022-06-14 01:02:32 +0900

    first svelte for oop_survey

---
+++ src/lib/content.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let q_cont;
+	let _title = q_cont.title;
+	let _type = q_cont.type;
+
+	delete q_cont.title;
+	delete q_cont.type;
+	let values = Object.keys(q_cont).map((k) => Object.keys(q_cont[k]).map((k2) => q_cont[k][k2]));
+</script>
+
+<div>
+	{#if _type == 'RadioButton'}
+		<h2>{_title}</h2>
+		{#each values[0] as inp}
+			<label>
+				<input type="radio" bind:group={_title} name={inp} value={_title} />
+				{inp}
+			</label>
+		{/each}
+	{:else}
+		<h2>{_title}</h2>
+		{#each values[0] as inp}
+			<label>
+				<input type="checkbox" />
+				{inp}
+			</label>
+		{/each}
+	{/if}
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script>
+	import 'carbon-components-svelte/css/white.css';
+	import Box from '$lib/Box.svelte';
+
+	async function getSurvey() {
+		let res = await fetch('http://php-server-ksu.run.goorm.io/PHP/', {});
+		let survey_db = await res.json();
+		return Object.values(survey_db);
+	}
+	const survey = getSurvey();
+</script>
+
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<div>
+	{#await survey}
+		<h3>Loading.getSurvey..</h3>
+	{:then survey_db}
+		{#each survey_db as { id, title }}
+			<Box>{id}님의 {title} 설문조사</Box>
+		{/each}
+	{:catch error}
+		<p>{error.message}</p>
+	{/await}
+</div>
